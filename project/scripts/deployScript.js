d6a43f8ea1028e78f4e461d9599e607a5cfa004f
Jaskeerat Singh <jaskeerat10000@gmail.com>
2022-02-04 22:05:04 +0530

    updated Greetor.sol and changed name from sample-scripts -> deployScripts & compiled the smart contract"

--- project/scripts/deployScript.js
+++ project/scripts/deployScript.js
@@ -2,12 +2,16 @@ const hre = require("hardhat");
 
 async function main() {
 
-  // We get the contract to deploy
+  // We get the reference to the contract which is to be deployed
   const Greeter = await hre.ethers.getContractFactory("Greeter");
+
+  // passed in the deploy method is the constructor text(greeting) which will be passed to the constructor in Greeter.sol
   const greeter = await Greeter.deploy("Hello, Hardhat!");
 
+  // waiting for the contract to be deployed
   await greeter.deployed();
 
+  // greeter.address is the address to which we will be deploying the contract
   console.log("Greeter deployed to:", greeter.address);
 }
 
